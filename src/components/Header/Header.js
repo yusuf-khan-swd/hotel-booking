b565d3663d5474687d1de8fcf170848e39a374bc
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-19 11:03:17 +0600

    added context

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../contexts/UserContext';
 
 const Header = () => {
+  const { user } = useContext(AuthContext);
   return (
     <div>
-      <h2>This is Header</h2>
+      <h2>This is Header for {user.displayName}</h2>
     </div>
   );
 };
