f5f778d9ec8f7ac12127065c515f1436348417ac
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-19 10:47:58 +0600

    added Header component

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from '../components/Header/Header';
 
 const Main = () => {
   return (
     <div>
-      <h2>This is main layout</h2>
+      <Header></Header>
       <Outlet></Outlet>
     </div>
   );
