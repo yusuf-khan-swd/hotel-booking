a2348516aa0e99da96dbed16723a40f03e2d2ce2
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-19 10:50:45 +0600

    added router provider to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './routes/routes/router';
 
 function App() {
   return (
     <div>
-
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
