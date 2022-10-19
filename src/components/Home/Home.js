9ccba57ef3d1461f3f2315f85e2161252f75a03e
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-19 18:36:01 +0600

    create a fake data to load in home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,18 @@ const Home = () => {
   return (
     <div>
       <h2>This is Home Component</h2>
+      <div>
+        <div className="card card-compact bg-base-100 shadow-xl">
+          <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">Single Person!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Book Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
