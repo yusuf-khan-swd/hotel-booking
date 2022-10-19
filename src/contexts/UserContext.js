dca3224e2b3c85cf566689aed81960e19b2de205
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-19 11:08:07 +0600

    Google Sign in added in context

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,15 +1,19 @@
-import React, { createContext } from 'react';
-import { getAuth } from 'firebase/auth';
+import React, { createContext, useState } from 'react';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const UserContext = ({ children }) => {
+  const [user, setUser] = useState({ displayName: 'Yusuf' });
+  const googleProvider = new GoogleAuthProvider();
 
-  const user = { displayName: 'Yusuf' };
+  const googleSignIn = () => {
+    signInWithPopup(auth, googleProvider);
+  };
 
-  const authInfo = { user };
+  const authInfo = { user, googleSignIn };
 
   return (
     <AuthContext.Provider value={authInfo}>
