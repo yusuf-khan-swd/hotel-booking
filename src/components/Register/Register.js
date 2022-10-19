c5d2981358d4578beb9db0224951ae2b65959c85
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-10-19 17:31:36 +0600

    remove dummy p text and lg:flex-row

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -58,14 +58,9 @@ const Register = () => {
         <p onClick={handleGoogleSignIn} className="btn btn-primary">Continue With Google</p>
       </div>
       <div className="hero min-h-screen lg:min-h-min bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="hero-content flex-col">
           <div className="text-center lg:text-left">
             <h1 className="text-5xl font-bold">Register now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut
-              assumenda excepturi exercitationem quasi. In deleniti eaque aut
-              repudiandae et a id nisi.
-            </p>
           </div>
           <form onSubmit={handleSubmit} className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 border">
             <div className="card-body">
